d9d641ac8e578aed286ee9a045e269cf369a2dfc
lcaputo <programadorcaputo@gmail.com>
2023-06-06 22:54:40 -0500

    feat: TypeOrmModule with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,28 @@ import { GroupModule } from './group/group.module';
 import { CareerModule } from './career/career.module';
 import { PersonTypeModule } from './person_type/person_type.module';
 import {TypeOrmModule}  from '@nestjs/typeorm';
+import { configuration } from './config/configuration';
+import { ConfigModule, ConfigService } from  '@nestjs/config/dist';
+
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type:'postgres',
-    username:'developer',
-    password:'1ngreso-3studiantes',
-    host:'ie.elprogramador.co',
-    port: 5432,
-    database:'IngresoEstudiantes',
-    entities:[__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_NAME'),
+        entities:[__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
   }), UserModule, UserRolesModule, RolesModule, PersonModule, GroupModule, CareerModule, PersonTypeModule,],
   controllers: [AppController],
   providers: [AppService],
