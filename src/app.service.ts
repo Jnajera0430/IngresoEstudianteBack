6dad394f2812464258d6d54effb9fba57705eca9
lcaputo <programadorcaputo@gmail.com>
2023-06-06 23:43:03 -0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
+import { ConfigService } from '@nestjs/config/dist';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    return this.configService.get('DB_HOST');
   }
 }
