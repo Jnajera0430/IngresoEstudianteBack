55990263f81263bad7d91736201e0a81749e424a
José A. Najera <auxsistemas3@sumidota.local>
2023-06-21 17:56:19 -0500

    feature: se añadio caracteristias para la documentacion en el swagger

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Get, Put, Post, Delete } from '@nestjs/common';
+import { Body, Controller, Get, Put, Post, Delete,Req } from '@nestjs/common';
 import { ApiBadRequestResponse, ApiBody, ApiConflictResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { bodyExampleCreateUser } from 'src/document/body.document';
-import { responseOkCreateUser } from 'src/document/responses.200';
+import { responseOkCreateUser, responseOkListUser } from 'src/document/responses.200';
 import { responseErrorExampleCreateUser400 } from 'src/document/responses.400';
 import { responseErrorServer } from 'src/document/responses.500';
 import { RoleDto } from 'src/dto/roles/rol.dto';
@@ -26,7 +26,7 @@ export class UserController {
     @ApiResponse(responseOkCreateUser())
     @ApiBadRequestResponse(responseErrorExampleCreateUser400())
     @ApiResponse(responseErrorServer())
-    createUser(@Body() newUser: CreateUserDto): Promise<User> {
+    createUser(@Req() req: Request,@Body() newUser: CreateUserDto): Promise<User> {
         return this.userService.createUser(newUser);
     }
 
@@ -35,96 +35,20 @@ export class UserController {
         summary: "Users list",
         description: 'Endpoint to list all users',
     })
-    @ApiResponse({
-        status: 200,
-        description: 'Users list with your type of roles',
-        content: {
-            'application/json': {
-                schema: {
-                    $ref: '#/components/schemas/CreateUserDto'
-                },
-                example: {
-                    "status": 200,
-                    message: "list users",
-                    data: [
-                        {
-                            "id": 21,
-                            "email": "example4@gm.com",
-                            "username": "exampleUser4",
-                            "createdAt": "2023-06-17T07:37:49.159Z",
-                            "state": true,
-                            "role": [
-                                {
-                                    "id": 1,
-                                    "tipo": "Super usuario"
-                                }
-                            ]
-                        },
-                        {
-                            "id": 22,
-                            "email": "example@gm.com",
-                            "username": "exampleUser",
-                            "createdAt": "2023-06-17T07:47:03.252Z",
-                            "state": true,
-                            "role": [
-                                {
-                                    "id": 1,
-                                    "tipo": "Super usuario"
-                                },
-                                {
-                                    "id": 2,
-                                    "tipo": "Administrador"
-                                },
-                                {
-                                    "id": 3,
-                                    "tipo": "Auditor"
-                                }
-                            ]
-                        },
-                        {
-                            "id": 24,
-                            "email": "example5@gm.com",
-                            "username": "exampleUser5",
-                            "createdAt": "2023-06-21T02:39:22.479Z",
-                            "state": true,
-                            "role": [
-                                {
-                                    "id": 1,
-                                    "tipo": "Super usuario"
-                                }
-                            ]
-                        },
-                        {
-                            "id": 26,
-                            "email": "example6@gm.com",
-                            "username": "exampleUser6",
-                            "createdAt": "2023-06-22T00:25:57.686Z",
-                            "state": true,
-                            "role": [
-                                {
-                                    "id": 1,
-                                    "tipo": "Super usuario"
-                                }
-                            ]
-                        }
-                    ]
-                }
-            }
-        }
-    })
+    @ApiResponse(responseOkListUser())
     @ApiBadRequestResponse(responseErrorExampleCreateUser400())
     @ApiResponse(responseErrorServer())
-    allUser(): Promise<User[]> {
+    allUser(@Req() req: Request): Promise<User[]> {
         return this.userService.findAll();
     }
 
     @Get(":id")
-    findOneById(id: number): Promise<User> {
+    findOneById(@Req() req: Request,id: number): Promise<User> {
         return this.userService.findOneById(id);
     }
 
     @Put()
-    update(req: Request, res: Response) {
+    update(@Req() req: Request, res: Response) {
 
     }
 
--- src/document/responses.200.ts
+++ src/document/responses.200.ts
@@ -1,56 +1,135 @@
-export const responseOkAuthUser=()=>({
-    status: 200,
-    content: {
-      'application/json': {
+export const responseOkAuthUser = () => ({
+  status: 200,
+  content: {
+    'application/json': {
 
-        example: {
-          "status": 200,
-          message: "success",
-          ok: true,
-          data: {
-            "message": "User successfully logged in"
-          }
-        }
-      }
-    },
-    headers: {
-      'Set-Cookie': {
-        description: 'Token de autenticación',
-        schema: {
-          type: 'string',
+      example: {
+        "status": 200,
+        message: "success",
+        ok: true,
+        data: {
+          "message": "User successfully logged in"
         }
       }
     }
-  })
+  },
+  headers: {
+    'Set-Cookie': {
+      description: 'Token de autenticación',
+      schema: {
+        type: 'string',
+      }
+    }
+  }
+})
 
-export const responseOkCreateUser=()=>(
+export const responseOkCreateUser = () => (
   {
     status: 201,
     description: 'user created',
     content: {
-        'application/json': {
-            schema: {
-                $ref: '#/components/schemas/CreateUserDto'
-            },
-            example: {
-                "status": 200,
-                message: "New user successfully created",
-                data: {
-                    "email": "example6@gm.com",
-                    "username": "exampleUser6",
-                    "password": "$2a$10$9Y1MDyA1NqtDuEo0GTgY1u4j0EQGZYNbQL1k51Zg3sDASw8XecTaa",
-                    "role": [
-                        {
-                            "id": 1,
-                            "tipo": "Super usuario"
-                        }
-                    ],
-                    "id": 26,
-                    "createdAt": "2023-06-22T00:25:57.686Z",
-                    "state": true
-                }
-            }
+      'application/json': {
+        schema: {
+          $ref: '#/components/schemas/CreateUserDto'
+        },
+        example: {
+          "status": 200,
+          message: "New user successfully created",
+          data: {
+            "email": "example6@gm.com",
+            "username": "exampleUser6",
+            "role": [
+              {
+                "id": 1,
+                "tipo": "Super usuario"
+              }
+            ],
+            "id": 26,
+            "createdAt": "2023-06-22T00:25:57.686Z",
+            "state": true
+          }
         }
+      }
     }
-}
-) 
+  }
+)
+
+export const responseOkListUser = () => (
+  {
+    status: 200,
+    description: 'Users list with your type of roles',
+    content: {
+      'application/json': {
+        schema: {
+          $ref: '#/components/schemas/CreateUserDto'
+        },
+        example: {
+          "status": 200,
+          message: "list users",
+          data: [
+            {
+              "id": 21,
+              "email": "example4@gm.com",
+              "username": "exampleUser4",
+              "createdAt": "2023-06-17T07:37:49.159Z",
+              "state": true,
+              "role": [
+                {
+                  "id": 1,
+                  "tipo": "Super usuario"
+                }
+              ]
+            },
+            {
+              "id": 22,
+              "email": "example@gm.com",
+              "username": "exampleUser",
+              "createdAt": "2023-06-17T07:47:03.252Z",
+              "state": true,
+              "role": [
+                {
+                  "id": 1,
+                  "tipo": "Super usuario"
+                },
+                {
+                  "id": 2,
+                  "tipo": "Administrador"
+                },
+                {
+                  "id": 3,
+                  "tipo": "Auditor"
+                }
+              ]
+            },
+            {
+              "id": 24,
+              "email": "example5@gm.com",
+              "username": "exampleUser5",
+              "createdAt": "2023-06-21T02:39:22.479Z",
+              "state": true,
+              "role": [
+                {
+                  "id": 1,
+                  "tipo": "Super usuario"
+                }
+              ]
+            },
+            {
+              "id": 26,
+              "email": "example6@gm.com",
+              "username": "exampleUser6",
+              "createdAt": "2023-06-22T00:25:57.686Z",
+              "state": true,
+              "role": [
+                {
+                  "id": 1,
+                  "tipo": "Super usuario"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  }
+)
