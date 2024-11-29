9f16864fd3a4dd39f408acbb3a912545cff57caa
lcaputo <programadorcaputo@gmail.com>
2024-11-29 00:10:08 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*",
+    origin: "https://pia.elprogramador.co",
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     preflightContinue: false,
